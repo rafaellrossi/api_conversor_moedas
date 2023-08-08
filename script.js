21d882f175c1fcb768ea73db572bab7b4dbad5c9
Rafael Rossi <91340426+rafaellrossi@users.noreply.github.com>
2023-08-07 22:52:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function converter() {
     let moedaA = document.getElementById('moedaA').value
     let moedaB = document.getElementById('moedaB').value
     let soma = 0
-    let url = `https://economia.awesomeapi.com.br/json/last/${moedaA}-${moedaB}`
+    let url = `https://economia.awesomeapi.com.br/json/${moedaA}-${moedaB}`
     fetch(url)
         .then((response) => response.json())
         .then(json => {
-            soma = (valor * json[moedaA].bid)
+            soma = (valor * json[0].bid)
             soma = soma.toFixed(2)
             let res = soma.replace(".", ",")
             document.getElementById('res1').innerHTML = "Resultado"
